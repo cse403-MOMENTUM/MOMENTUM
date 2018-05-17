0d51c29f9a598662d3b8d8d782565c17dcf51274
Anita Leung <leungak@cs.washington.edu>
2018-05-16 19:18:23 -0700

    add dropdown to assign to members on task input

--- momentum/src/views/presentationals/TaskInput.tsx
+++ momentum/src/views/presentationals/TaskInput.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { Control, Form } from 'react-redux-form';
-import { Field } from 'redux-form';
-import { Form as SemForm } from 'semantic-ui-react';
+import { Form } from 'semantic-ui-react';
 import { Task } from 'src/models/Task';
+// import '../styles/Task.css';
 
 // import logo from '../data/logo.svg';
 
@@ -11,6 +10,12 @@ interface Props {
   handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   handleOnChange: (event: React.FormEvent<HTMLInputElement>) => void;
 }
+
+const teamMemberOptions = [
+  { key: 'anitaleung', text: 'Anita Leung', value: 'anitaleung'},
+  { key: 'austinha', text: 'Austin Ha', value: 'austinha'},
+  { key: 'joshpollock', text: 'Josh Pollock', value: 'joshpollock'}
+];
 
 class TaskInput extends React.Component<Props, {}> {
   constructor(props: Props) {
@@ -22,35 +27,36 @@ class TaskInput extends React.Component<Props, {}> {
       <div className="task-detail">
         <header className="task-detail-header">
           <h1 className="task-detail-title">This task name: { this.props.currentTask.name }</h1>
-          <Form onSubmit={this.props.handleSubmit} model="task">
+          <Form onSubmit={ this.props.handleSubmit }>
 
-            <Control.text model=".name" />
-
-            <Field
-              name="task-name"
+            <Form.Input
               inline={true}
               fluid={true}
               label="Task Name"
               placeholder="Task name"
               onChange={ this.props.handleOnChange }/>
 
-            <SemForm.Group inline={true} id="task-priority">
+            <Form.Group inline={true} id="task-priority">
               <label htmlFor="taskImportance">Priority level</label>
 
-              <SemForm.Radio label="Low" value="l" />
-              <SemForm.Radio label="Medium" value="m" />
-              <SemForm.Radio label="High" value="h" />
-            </SemForm.Group>
+              <Form.Radio label="Low" value="l" />
+              <Form.Radio label="Medium" value="m" />
+              <Form.Radio label="High" value="h" />
+            </Form.Group>
 
-            <SemForm.Group inline={true}>
-              <SemForm.Input inline={true} label="Your task time estimate" /> hours
-            </SemForm.Group>
+            <Form.Group inline={true}>
+              <Form.Input inline={true} label="Your task time estimate" /> hours
+            </Form.Group>
 
-            <SemForm.TextArea
+            <Form.Select label="Assign to"
+              options={teamMemberOptions}
+              placeholder="Team member" />
+
+            <Form.TextArea
               label="Task description"
               placeholder="Enter your task description" />
 
-            <SemForm.Button color="blue" type="submit">Create this task</SemForm.Button>
+            <Form.Button color="blue" type="submit">Create this task</Form.Button>
           </Form>
         </header>
       </div>
