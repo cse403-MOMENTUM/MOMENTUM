436dffeab7e98c693ad377f97a89898b4d01b8f5
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-06-05 22:00:20 -0700

    revise dotplot viz

--- momentum/src/components/DotPlots/NormalDistribution.tsx
+++ momentum/src/components/DotPlots/NormalDistribution.tsx
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 import * as React from 'react';
 import '../../styles/DotPlots.css';
 import DotPlot from './DotPlot';
-import Graph from './Graph';
+// import Graph from './Graph';
 
 interface Props {
   margin: {
@@ -19,11 +19,11 @@ interface Props {
 class NormalDistribution extends React.Component<Props, {}> {
   // taken from Jason Davies science library
   // https://github.com/jasondavies/science.js/blob/master/src/stats/distribution/gaussian.js
-  private static gaussian_pdf(x: number, mean: number, sigma: number) {
-    const gaussianConstant = 1 / Math.sqrt(2 * Math.PI);
-    const adjustedX = (x - mean) / sigma;
-    return gaussianConstant * Math.exp(-.5 * adjustedX * adjustedX) / sigma;
-  }
+  // private static gaussian_pdf(x: number, mean: number, sigma: number) {
+  //   const gaussianConstant = 1 / Math.sqrt(2 * Math.PI);
+  //   const adjustedX = (x - mean) / sigma;
+  //   return gaussianConstant * Math.exp(-.5 * adjustedX * adjustedX) / sigma;
+  // }
 
   public ref: SVGElement;
 
@@ -33,7 +33,7 @@ class NormalDistribution extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props);
     this.xScale = d3.scaleLinear()
-                    .domain([0 - (this.props.z_limit * 1), 0 + (this.props.z_limit * 1)])
+                    .domain([10 - (this.props.z_limit * 1), 10 + (this.props.z_limit * 1)])
                     .range([0, this.props.width]);
     this.yScale = d3.scaleLinear()
                    //  .domain([0,.6])
@@ -50,9 +50,9 @@ class NormalDistribution extends React.Component<Props, {}> {
     const paddedWidth = width + margin.left + margin.right;
     const paddedHeight = height + margin.top + margin.bottom;
 
-    function graphFun(x: number) {
-      return NormalDistribution.gaussian_pdf(x, 0, 1);
-    }
+    // function graphFun(x: number) {
+    //   return NormalDistribution.gaussian_pdf(x, 0, 1);
+    // }
 
     return (
       <svg width={paddedWidth} height={paddedHeight}>
